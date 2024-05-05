2348ff7881f68ca998e2fa19366a98b7500303b9
Shirish Maharjan <sirimhrzn@gmail.com>
2024-05-05 13:12:10 +0545

    add: add rsyslog nginx and oci-container for nginx

--- flake.nix
+++ flake.nix
@@ -3,34 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
-    nixpkgs-unstable.url =  "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
   };
-
-  outputs = { self, nixpkgs, nixpkgs-unstable }:
-  let 
-     system = "x86_64-linux";
-     pkgs = import nixpkgs {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
         inherit system;
         config = {
-           allowUnfree = true;
+          allowUnfree = true;
         };
-     };
-  in
-  {
-     nixosConfigurations = {
-      sirimhrzn = nixpkgs.lib.nixosSystem {
-	specialArgs = { inherit system; };
-	modules =
-	      let
-        	defaults = { pkgs, ... }: {
-          	_module.args.nixpkgs-unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
-        	};
-	      in
- 	[
-		defaults
-		./nixos/configuration.nix
-	];
       };
-     };
-  };
+    in
+    {
+      nixosConfigurations = {
+        sirimhrzn = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit system;
+          };
+          modules =
+            let
+              defaults =
+                { pkgs, ... }:
+                {
+                  _module.args.nixpkgs-unstable = import nixpkgs-unstable {
+                    inherit system;
+                    config.allowUnfree = true;
+                  };
+                };
+            in
+            [
+              defaults
+              ./nixos/configuration.nix
+            ];
+        };
+      };
+    };
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,174 +2,205 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, nixpkgs-unstable ,... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  config,
+  pkgs,
+  nixpkgs-unstable,
+  ...
+}:
+{
+  imports = [ ./hardware-configuration.nix ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   nixpkgs.config.allowUnfree = true;
-  # nixpkgs.config.packageOverrides = pkgs: {
-  #   nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-  #     inherit pkgs;
-  #   };
-  # };
+
   networking = {
-      hostName = "nixos";
+    hostName = "nixos";
   };
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
-  virtualisation.docker.enable = true;
   virtualisation.libvirtd.enable = true;
   boot.kernelModules = [ "kvm-intel" ];
 
-programs.starship = {
-   enable = true;
-};
-fonts.packages =  [
-  pkgs.noto-fonts
-  pkgs.noto-fonts-cjk
-  pkgs.noto-fonts-emoji
-  pkgs.liberation_ttf
-  pkgs.fira-code
-  pkgs.fira-code-symbols
-  pkgs.mplus-outline-fonts.githubRelease
-  pkgs.dina-font
-  pkgs.proggyfonts
-  pkgs.noto-fonts-emoji
-  (nixpkgs-unstable.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono" "ZedMono"]; })
-];
+  programs.starship = {
+    enable = true;
+  };
+  fonts.packages = [
+    pkgs.noto-fonts
+    pkgs.noto-fonts-cjk
+    pkgs.noto-fonts-emoji
+    pkgs.liberation_ttf
+    pkgs.fira-code
+    pkgs.fira-code-symbols
+    pkgs.mplus-outline-fonts.githubRelease
+    pkgs.dina-font
+    pkgs.proggyfonts
+    pkgs.noto-fonts-emoji
+    (nixpkgs-unstable.nerdfonts.override {
+      fonts = [
+        "FiraCode"
+        "DroidSansMono"
+        "JetBrainsMono"
+        "ZedMono"
+      ];
+    })
+  ];
 
-programs.zsh.enable = true;
-services.pipewire = {
-  enable = true;
-  alsa.enable = true;
-  alsa.support32Bit = true;
-  pulse.enable = true;
-  jack.enable = true;
-};
+  programs.zsh.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
   networking.networkmanager.enable = true;
   time.timeZone = "Asia/Kathmandu";
   i18n.defaultLocale = "en_US.UTF-8";
 
   hardware = {
-	opengl.enable = true;
-	nvidia.modesetting.enable = true;
-};
-  programs.hyprland = {
-  	enable = true;
-  	xwayland.enable = true;
+    opengl.enable = true;
+    nvidia.modesetting.enable = true;
   };
 
-   services.xserver = {
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  services.xserver = {
     enable = true;
     layout = "us";
-    xkbVariant = "";
-  displayManager.gdm = {
-	enable = true;
-        wayland = true;
-	};
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
   };
 
   xdg.portal.enable = true;
   xdg.autostart.enable = true;
-  xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-hyprland xdg-desktop-portal xdg-desktop-portal-gtk];
+  xdg.portal.extraPortals = with pkgs; [
+    xdg-desktop-portal-hyprland
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+  ];
 
+  users.groups.sirimhrzn = { };
   users.users.sirimhrzn = {
     isNormalUser = true;
+    group = "sirimhrzn";
     description = "sirimhrzn";
     hashedPassword = "$6$gj7wGP2bivbhIQQ3$b22ysKyYOMXlrPh0iRhAUHAhOE2wC1uVrhbBMKdYAnH3wedZfqPP5ho6op8kHFLNed4p3lR47mnoH6ib/sYK31";
-    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd"];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+      "libvirtd"
+    ];
     packages = [
-    pkgs.firefox-wayland
-    nixpkgs-unstable.brave
-    pkgs.networkmanagerapplet
-    pkgs.openvpn
-    pkgs.font-awesome
-    pkgs.nerdfonts
-    pkgs.oh-my-zsh
-    pkgs.acpi
-    pkgs.gnome.nautilus
-    pkgs.gitui
-    pkgs.cargo 
-    pkgs.bat 
-    pkgs.nodejs_21
-    pkgs.nodePackages.npm
-    pkgs.pavucontrol
-    pkgs.blueman
-    pkgs.bluez
-    pkgs.bluez-tools
-    pkgs.swappy
-    pkgs.grimblast
-    pkgs.ripgrep
-    pkgs.pamixer
-    pkgs.discord
-    pkgs.docker
-    pkgs.polybar
-    pkgs.perl
-    pkgs.ranger
-    pkgs.xclip
-    pkgs.silicon
-    pkgs.starship
-    pkgs.feh
-    pkgs.btop
-    pkgs.neofetch
-    nixpkgs-unstable.gleam
-    nixpkgs-unstable.go
-    nixpkgs-unstable.gopls
-    nixpkgs-unstable.obsidian
-    nixpkgs-unstable.glab
-    nixpkgs-unstable.postman
-    nixpkgs-unstable.helix
-    nixpkgs-unstable.qutebrowser
-    nixpkgs-unstable.vscode
-    nixpkgs-unstable.alacritty
-    nixpkgs-unstable.erlang
-    nixpkgs-unstable.git-cliff
-    nixpkgs-unstable.diff-so-fancy
-    nixpkgs-unstable.wezterm
-    nixpkgs-unstable.bun
-    nixpkgs-unstable.neovim
+      pkgs.firefox-wayland
+      pkgs.networkmanagerapplet
+      pkgs.openvpn
+      pkgs.font-awesome
+      pkgs.nerdfonts
+      pkgs.oh-my-zsh
+      pkgs.acpi
+      pkgs.gnome.nautilus
+      pkgs.gitui
+      pkgs.cargo
+      pkgs.bat
+      pkgs.nodejs_21
+      pkgs.pavucontrol
+      pkgs.blueman
+      pkgs.bluez
+      pkgs.bluez-tools
+      pkgs.swappy
+      pkgs.grimblast
+      pkgs.ripgrep
+      pkgs.pamixer
+      pkgs.discord
+      pkgs.docker
+      pkgs.silicon
+      pkgs.starship
+      pkgs.btop
+      pkgs.neofetch
+      pkgs.minikube
+      nixpkgs-unstable.brave
+      nixpkgs-unstable.nodePackages.npm
+      nixpkgs-unstable.go
+      nixpkgs-unstable.gopls
+      nixpkgs-unstable.glab
+      nixpkgs-unstable.helix
+      nixpkgs-unstable.qutebrowser
+      nixpkgs-unstable.vscode
+      nixpkgs-unstable.alacritty
+      nixpkgs-unstable.git-cliff
+      nixpkgs-unstable.delta
+      nixpkgs-unstable.wezterm
+      nixpkgs-unstable.bun
+      nixpkgs-unstable.neovim
     ];
   };
   programs.direnv = {
     enable = false;
   };
-  nix.settings.trusted-users = [ "root" "sirimhrzn" ];
+  nix.settings.trusted-users = [
+    "root"
+    "sirimhrzn"
+  ];
   # programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
-#   users.users.sirimhrzn.openssh.authorizedKeys.keys = [
-# ];
-#
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+
+  services.nginx =
+    let
+      data_dir = "/home/sirimhrzn/Desktop/Genius/news/public";
+    in
+    {
+      enable = true;
+      recommendedProxySettings = true;
+      virtualHosts.localhost = {
+        extraConfig = ''
+          add_header x-systemd-nginx yes;
+        '';
+        locations."/" = {
+          root = data_dir;
+          extraConfig = ''
+            proxy_pass http://localhost:8080;
+          '';
+        };
+      };
+    };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      80
+      443
+    ];
+  };
+
   environment.systemPackages = [
-    pkgs.vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    pkgs.vim
     pkgs.wget
     pkgs.dunst
     pkgs.libnotify
     pkgs.waybar
     pkgs.swww
-    nixpkgs-unstable.kitty
     pkgs.polkit_gnome
     pkgs.dolphin
     pkgs.ntfs3g
     pkgs.wl-clipboard
     pkgs.curl
     pkgs.zsh
+    pkgs.fluent-bit
     pkgs.brightnessctl
     pkgs.wofi
     pkgs.fish
@@ -179,28 +210,47 @@ services.pipewire = {
     pkgs.kubectl
     pkgs.xclip
     pkgs.clang
+    pkgs.rsyslog
     pkgs.openssl.dev
+    pkgs.php82
+    pkgs.nginx
+    nixpkgs-unstable.kitty
     nixpkgs-unstable.zellij
     nixpkgs-unstable.rust-script
   ];
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  services.rsyslogd = {
+    enable = true;
+    extraConfig = ''
+      $ModLoad imtcp
+      $InputTCPServerRun 514
+      if $programname == 'laravel' then /var/log/laravel.log
+      if $programname == 'nginx' then /var/log/nginx.log
+    '';
+  };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+  };
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      nginx =
+        let
+          dir = "/home/sirimhrzn/nixos-containers/nginx";
+        in
+        {
+          image = "nginx:stable-alpine";
+          ports = [ "8080:80" ];
+          volumes = [
+            "${dir}/nginx.conf:/etc/nginx/nginx.conf"
+            "${dir}/conf.d:/etc/nginx/conf.d"
+            "${dir}/log:/var/log/nginx"
+          ];
+          autoStart = true;
+        };
+    };
+  };
   system.stateVersion = "23.11";
-
 }
